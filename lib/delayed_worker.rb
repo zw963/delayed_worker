fbcfb3362ee3f609be73024c3363bdc9814ddb76
Billy.Zheng <zw963@163.com>
2018-02-13 17:14:07 +0800

    Add Rakefile, can run task as: rake test

--- lib/delayed_worker.rb
+++ lib/delayed_worker.rb
@@ -1,7 +1,20 @@
-#!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
-module DelayedWorker
-  # code goes here...
-end
+require 'delayed_worker/version'
 
+class DelayedWorker
+  include Sidekiq::Worker
+  sidekiq_options backtrace: true, queue: 'promotion'
+
+  def perform(ar_id, ar_type, callback, params)
+    logger.info("\033[0;33m开始执行后台任务!\033[0m")
+    type = ar_type.constantize
+    if type < ActiveRecord::Base
+      ar_object = type.find(ar_id)
+      ar_object.instance_eval(callback, __FILE__, __LINE__)
+    else
+      instance_eval(callback, __FILE__, __LINE__)
+    end
+    Rails.logger.info("\033[0;33m后台任务执行完成!\033[0m")
+  end
+end
---
+++ test/delayed_worker_test.rb
@@ -0,0 +1,6 @@
+class DelayedWorkerTest < Minitest::Test
+  def test_ok
+    assert_equal 100, 100
+  end
+end
+
