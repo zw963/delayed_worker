45846d3cfc349ff9607d1748829546e9eb158e86
Billy.Zheng <zw963@163.com>
2018-02-14 01:44:04 +0800

    refactor

--- lib/delayed_worker/delayed_worker.rb
+++ lib/delayed_worker/delayed_worker.rb
@@ -4,14 +4,14 @@ class DelayedWorker
   include Sidekiq::Worker
   sidekiq_options backtrace: true, queue: 'default'
 
-  def perform(job_name, ar_id, ar_type, callback, params)
-    delayed_worker_log "Delayed worker:\033[0;33m #{job_name} \033[0mis start!"
-
-    type = Object.const_get(ar_type)
+  def perform(job_name, subject_id, subject_type, callback, params)
+    type = Object.const_get(subject_type)
     if type < ActiveRecord::Base
-      record = type.find(ar_id)
+      record = type.find(subject_id)
+      delayed_worker_log "Delayed worker:\033[0;33m #{job_name} \033[0mis start!"
       record.instance_eval(callback, __FILE__, __LINE__)
     else
+      delayed_worker_log "Delayed worker:\033[0;33m #{job_name} \033[0mis start!"
       instance_eval(callback, __FILE__, __LINE__)
     end
 
